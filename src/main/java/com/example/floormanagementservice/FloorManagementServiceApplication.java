8440b5dfbc9d9e6254a08720d9a262151e3eeae1
rum3r <kvirk2561@gmail.com>
2023-02-24 22:55:10 +0530

    initial commit

---
+++ src/main/java/com/example/floormanagementservice/FloorManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.floormanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloorManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloorManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/floormanagementservice/FloorManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.floormanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloorManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
